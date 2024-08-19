ab0d230bd02dfb39d210a63c086fa04c99c8a178
matzajdel <mateusz.zajdel111@gmail.com>
2024-08-19 11:14:31 +0200

    React redux: notesApiSlice

--- frontend/src/features/notes/NotesList.js
+++ frontend/src/features/notes/NotesList.js
@@ -1,9 +1,51 @@
 import React from 'react'
+import { useGetNotesQuery } from './notesApiSlice'
+import Note from './Note.js'
 
 const NotesList = () => {
-  return (
-    <div>NotesList</div>
-  )
+  
+  let content
+
+  const {
+    data: notes,
+    isLoading,
+    isSuccess,
+    isError,
+    error
+  } = useGetNotesQuery()
+
+  if (isLoading)  content = <p>Loading....</p>
+  
+  if (isError)    content = <p className='errmsg'>{error?.data?.message}</p>
+  
+  if (isSuccess)
+  {
+      const tableContent = notes?.ids?.length
+      ?   notes.ids.map(noteId => 
+            <Note key={noteId} noteId={noteId} />
+          )
+      :   null    
+    
+      content = (
+      <table className="table table--notes">
+          <thead className="table__thead">
+              <tr>
+                  <th scope="col" className="table__th note__status">Status</th>
+                  <th scope="col" className="table__th note__created">Created</th>
+                  <th scope="col" className="table__th note__updated">Updated</th>
+                  <th scope="col" className="table__th note__title">Title</th>
+                  <th scope="col" className="table__th note__username">Owner</th>
+                  <th scope="col" className="table__th note__edit">Edit</th>
+              </tr>
+          </thead>
+          <tbody>
+              {tableContent}
+          </tbody>
+      </table>
+      )
+  }
+
+  return content
 }
 
 export default NotesList
